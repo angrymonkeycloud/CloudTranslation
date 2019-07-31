6ef018eb511747c9c5f27876548533f2315adac4
Elie Tebchrani <elietebchrani@angrymonkeyagency.com>
2019-07-31 14:09:07 +0300

    Versioning added to distribution files

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,42 @@
 var gulp = require('gulp');
 var sourcemaps = require('gulp-sourcemaps');
 var minify = require('gulp-minify');
+var rename = require('gulp-rename');
 var ts = require('gulp-typescript');
 var tsProject = ts.createProject('tsconfig.json');
 
-gulp.task('default', function() {
+gulp.task('default', gulp.series('withoutVersioning', 'withVersioning'));
+
+gulp.task('withoutVersioning', function() {
     return tsProject.src()
         .pipe(tsProject())
         .js
         .pipe(sourcemaps.init({ loadMaps: true }))
-        .pipe(minify())
+        .pipe(minify({
+            ext: {
+                min: '.min.js'
+            }
+        }))
+        .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest('dist'));
+
+});
+
+gulp.task('withVersioning', function() {
+
+    // Please don't modify
+    var version = '1.0.0';
+
+    return tsProject.src()
+        .pipe(tsProject())
+        .js
+        .pipe(rename('cloudtranslation-' + version + '.js'))
+        .pipe(sourcemaps.init({ loadMaps: true }))
+        .pipe(minify({
+            ext: {
+                min: '.min.js'
+            }
+        }))
         .pipe(sourcemaps.write('./'))
         .pipe(gulp.dest('dist'));
 });
